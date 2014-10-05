c19b4376f1abf9b18c8a6159ebf2ecb5da166ffb
Wendan Kang <Wendan@KangWendans-MacBook-Pro.local>
2014-10-04 21:45:03 -0400

    syscall 378 return 0 test3

--- flo-kernel/kernel/acceleration.c
+++ flo-kernel/kernel/acceleration.c
@@ -42,7 +42,7 @@ struct dev_acceleration {
  struct dev_acceleration __user * acceleration
  */
  
-SYSCALL_DEFINE1(set_acceleration, int, test) {
+SYSCALL_DEFINE1(set_acceleration, struct dev_acceleration __user *, acceleration) {
 	return test;
 }
 
