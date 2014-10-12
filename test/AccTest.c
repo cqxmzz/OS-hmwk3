10b3b4c2002e902646b363e6f1c1a36bdb5a3106
to0 <yuanancai@gmail.com>
2014-10-12 16:49:08 -0400

    distinguish shakes along x and y

--- test/AccTest.c
+++ test/AccTest.c
@@ -8,19 +8,27 @@
 
 #define MAX 20
 
-int launchOneDetector(int eventId) {
+int launchOneDetector(int eventId, struct acc_motion motion) {
 	pid_t pid = fork();
 	if (pid < 0) {
-	exit(1);
+		exit(1);
 	}
 	if(pid == 0) {
 		/* Block a process on an event. */
-		syscall(380, eventId);
-		printf("%d detected a shake\n", getpid());
+		if(syscall(380, eventId) == 0) {
+			if(motion.dlt_x != 0 && motion.dlt_y == 0 && motion.dlt_z == 0) {
+				printf("%d detected a horizontal shake\n", getpid());
+			}
+			else if(motion.dlt_x == 0 && motion.dlt_y != 0 && motion.dlt_z == 0) {
+				printf("%d detected a vertical shake\n", getpid());
+			}
+			else {
+				printf("%d detected a shake\n", getpid());
+			}
+		}
 		exit(0);
 		// exit(syscall(380, eventId));
 	}
-
 	return 0;
 }
 int main(int argc, const char *argv[]) {
@@ -29,12 +37,13 @@ int main(int argc, const char *argv[]) {
 	unsigned int x, y, z, frq;
 	struct acc_motion motion;
 	FILE* input;
+	int i;
 	if((input = fopen("input", "r")) != NULL){
 		fscanf(input, "%i\n", &numberOfDetectors);
 		if(numberOfDetectors > MAX){
 			numberOfDetectors = MAX;
 		}
-		int i;
+
 		for(i = 0; i < numberOfDetectors; i++) {
 			fscanf(input, "%u %u %u %u\n", &x, &y, &z, &frq);
 			motion.dlt_x = x;
@@ -43,9 +52,9 @@ int main(int argc, const char *argv[]) {
 			motion.frq = frq;
 			/* Create an event based on motion. */
 			eventQueue[i] = syscall(379, &motion);
-			launchOneDetector(eventQueue[i]);
+			launchOneDetector(eventQueue[i], motion);
 		}
-		sleep(15);
+		sleep(65);
 
 		for(i = 0; i < numberOfDetectors; i++) {
 			syscall(382, eventQueue[i]);
