b228da0517a3adb78843b696f67e737974b82303
Qiming Chen <qc2195@columbia.edu>
2014-10-14 10:51:27 -0400

    correct lock behaviur

--- flo-kernel/kernel/acc_sync.c
+++ flo-kernel/kernel/acc_sync.c
@@ -185,15 +185,15 @@ SYSCALL_DEFINE1(accevt_wait, int, event_id) {
 		return -EINVAL;
 	}
 	array.structs[event_id].waking_num++;
-	spin_unlock(&my_lock);
 	add_wait_queue(array.structs[event_id].waking, &wait1);
 	while (array.structs[event_id].flag == 1) {
+		spin_unlock(&my_lock);
 		prepare_to_wait(array.structs[event_id].waking, &wait1
 			, TASK_INTERRUPTIBLE);
 		schedule();
+		spin_lock(&my_lock);
 	}
 	finish_wait(array.structs[event_id].waking, &wait1);
-	spin_lock(&my_lock);
 	array.structs[event_id].waking_num--;
 	if (array.structs[event_id].flag == 2) {
 		if (array.structs[event_id].waking_num == 0) {
@@ -204,16 +204,15 @@ SYSCALL_DEFINE1(accevt_wait, int, event_id) {
 		return -EINVAL;
 	}
 	array.structs[event_id].num++;
-	spin_unlock(&my_lock);
-
 	add_wait_queue(array.structs[event_id].q, &wait2);
 	while ((ret = array.structs[event_id].flag) == 0) {
+		spin_unlock(&my_lock);
 		prepare_to_wait(array.structs[event_id].q, &wait2
 			, TASK_INTERRUPTIBLE);
 		schedule();
+		spin_lock(&my_lock);
 	}
 	finish_wait(array.structs[event_id].q, &wait2);
-	spin_lock(&my_lock);
 	array.structs[event_id].num--;
 	if (array.structs[event_id].flag == 2
 		&& array.structs[event_id].num == 0) {
